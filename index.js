5288829d89cefa4233c53b11de6ebb1e9037fa80
Thoams Krg <thoams.krieg@gmail.com>
2024-02-19 19:43:58 +0100

    TP Fin ok

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
+const sidebar = document.getElementById("side-bar");
+const content = document.querySelector(".content");
+
+btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+content.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+});
+
+// Créer un événement au click sur le hamburger menu pour sortir la sidebar et assombrir le fond, ranger la sidebar si on reclick dessus et rééclairer le fond
 
 // Ranger la sidebar si on click sur le contenu principal
 
